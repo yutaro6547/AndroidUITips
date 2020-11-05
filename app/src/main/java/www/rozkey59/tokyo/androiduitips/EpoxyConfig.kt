f1adda17df1ab8d71dd00852669c70a9b1cc198e
zukkey <>
2020-11-05 11:17:06 +0900

    Update Versions

---
+++ app/src/main/java/www/rozkey59/tokyo/androiduitips/EpoxyConfig.kt
@@ -0,0 +1,6 @@
+package www.rozkey59.tokyo.androiduitips
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "item_")
+interface EpoxyConfig
